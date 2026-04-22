0d56da0f3287c63efc85deaa96d7e53aca06f466
Juan Cruz <juangomezcorbalan@gmail.com>
2026-04-22 19:37:32 -0300

    se agrego la funcion buscarNombre en la rama main

--- ArregloDeNombres.c
+++ ArregloDeNombres.c
@@ -9,13 +9,18 @@
 //-----DECLARACION DE FUNCIONES-----//
 void cargarPersonas(char *V[]);
 void mostrarPersonas(char *V[]);
+void buscarNombre(char *V[],char palabra[]);
 
 //-----MAIN-----//
 int main()
 {
     char *nombres[cant_alumnos];
     cargarPersonas(nombres);
-    mostrarPersonas(nombres);
+    //mostrarPersonas(nombres);
+    char palabra[20];int result;
+    printf("Ingrese un nombre para realizar la busqueda:");
+    scanf("%s", palabra);
+    buscarNombre(nombres,palabra);
 }
 
 //------FUNCIONES-----//
@@ -23,7 +28,7 @@ void cargarPersonas(char *V[])
 {
     int i;
     char buff[20];
-    int longitud = strlen(buff);
+    int longitud;
     for (i = 0; i < cant_alumnos; i++)
     {
         printf("Ingrese el nombre %d:\n", (i + 1));
@@ -40,5 +45,19 @@ void mostrarPersonas(char *V[])
     for (i = 0; i < cant_alumnos; i++)
     {
         printf("\nNombre %d: %s", (i + 1), V[i]);
+    }
+}
+
+void buscarNombre(char *V[],char palabra[]){
+    int i=0;
+    int seEncontro=0;
+    char *resultado;
+    while(seEncontro!=1 && cant_alumnos>i){
+        resultado=strstr(V[i],palabra);
+        if(resultado!=NULL){
+            printf("Coincide con: %s",V[i]);
+            seEncontro=1;
+        }
+        i++;
     }
 }
